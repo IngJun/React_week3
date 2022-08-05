b392ce3a2fdaaca2d68694cdd5c0f1b9b62c39d3
darksilicon13 <darksilicon@outlook.com>
2022-08-05 18:23:27 +0900

    Redux resource created

---
+++ src/redux/configStore.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import posts from './modules/posts'
+
+const rootReducer = combineReducers({ posts });
+
+const store = configureStore({ reducer: rootReducer });
+
+export default store;
--- src/redux/modules/posts.js
+++ src/redux/modules/posts.js
@@ -3,23 +3,152 @@
 const initialState = {
     list: [
         {
-            title:'post example',
+            id: '1',
+            title: 'post example',
             content: 'content example',
-
+            comments: [
+                { id: '1', content: 'comments example 1' },
+                { id: '2', content: 'comments example 2' },
+                { id: '3', content: 'comments example 3' },
+                { id: '4', content: 'comments example 4' }
+            ],
         }
     ],
-    currentPost:{},
+    currentPost: {},
+    currentComment: {},
 };
 
 
 // action types
 const CREATEPOST = 'posts/createPost';
+const READPOST = 'posts/readPost';
 const UPDATEPOST = 'posts/updatePost';
 const DELETEPOST = 'posts/deletePost';
 
 const CREATECOMMENT = 'posts/createComment';
+const READCOMMENT = 'posts/readComment';
 const UPDATECOMMENT = 'posts/updateComment';
 const DELETECOMMENT = 'posts/deleteComment';
 
 
 // action creators
+
+// post = {id='', title: '', content: ''}
+export const createPost = (post) => {
+    return { type: CREATEPOST, post };
+}
+
+export const readPost = (post_id) => {
+    return { type: READPOST, post_id };
+}
+
+// post = {title: '', content: ''}
+export const updatePost = (post_id, post) => {
+    return { type: UPDATEPOST, post_id, post };
+}
+
+export const deletePost = (post_id) => {
+    return { type: DELETEPOST, post_id };
+}
+
+// comment = {id: '', content: ''}
+export const createComment = (comment, post_id) => {
+    return { type: CREATECOMMENT, comment, post_id };
+}
+
+export const readComment = (comment_id, post_id) => {
+    return { type: READCOMMENT, comment_id, post_id };
+}
+
+// comment = {content: ''}
+export const updateComment = (post_id, comment_id, comment) => {
+    return { type: UPDATECOMMENT, post_id, comment_id, comment };
+}
+
+export const deleteComment = (post_id, comment_id) => {
+    return { type: DELETECOMMENT, post_id, comment_id };
+}
+
+
+// reducer
+
+export default function reducer(state = initialState, action = {}) {
+    switch (action.type) {
+        case CREATEPOST: {
+            console.log('creating post');
+            const new_post = { ...action.post, comment: [] };
+            const new_post_list = [...state.list, new_post];
+            return { ...state, list: new_post_list };
+        }
+        case READPOST: {
+            console.log('reading post');
+            const current_post = state.list.find((post) => post.id === action.post_id);
+            return { ...state, currentPost: current_post };
+        }
+        case UPDATEPOST: {
+            console.log('updating post');
+            const updated_post_list = state.list.map((post) => {
+                if (post.id === action.post_id) {
+                    return { ...post, title: action.post.title, content: action.post.content }
+                } else {
+                    return post;
+                }
+            })
+            return { ...state, list: updated_post_list };
+        }
+        case DELETEPOST: {
+            console.log('deleting post');
+            const new_post_list = state.list.filter((post) => post.id !== action.post_id);
+            return { ...state, list: new_post_list };
+        }
+        case CREATECOMMENT: {
+            console.log('creating comment');
+            const new_post_list = state.list.map((post) => {
+                if (post.id === action.post_id) {
+                    const new_comments = [...post.comments, action.comment];
+                    return { ...post, comments: new_comments };
+                } else {
+                    return post;
+                }
+            })
+            return { ...state, list: new_post_list };
+        }
+        case READCOMMENT: {
+            console.log('reading comment');
+            const current_post = state.list.find((post) => post.id === action.post_id);
+            const current_comment = current_post.comments.find((comment) => comment.id === action.comment_id);
+            return { ...state, currentComment: current_comment };
+        }
+        case UPDATECOMMENT: {
+            console.log('updating comment');
+            const new_post_list = state.list.map((post) => {
+                if (post.id === action.post_id) {
+                    const new_comments = post.comments.map((comment) => {
+                        if (comment.id === action.comment_id) {
+                            return { ...comment, content: action.content };
+                        } else {
+                            return comment;
+                        }
+                    });
+                    return { ...post, comment: new_comments };
+                } else {
+                    return post;
+                }
+            })
+            return { ...state, list: new_post_list };
+        }
+        case DELETECOMMENT: {
+            console.log('deleting comment');
+            const new_post_list = state.list.map((post) => {
+                if(post.id === action.post_id) {
+                    const new_comments = post.comments.filter((comment) => comment.id !== action.comment_id);
+                    return {...post, comments: new_comments};
+                } else {
+                    return post;
+                }
+            }) 
+            return {...state, list: new_post_list};
+        }
+        default: return state;
+    }
+}
