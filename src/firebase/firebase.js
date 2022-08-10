2bf7d5f292048b2641dbc8c9357d4f10edb4d2e8
darksilicon13 <darksilicon@outlook.com>
2022-08-10 10:49:43 +0900

    firebase initialized

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FS_API_KEY,
+    authDomain: "sparta-react-advanced-e25c7.firebaseapp.com",
+    projectId: "sparta-react-advanced-e25c7",
+    storageBucket: "sparta-react-advanced-e25c7.appspot.com",
+    messagingSenderId: "199766521789",
+    appId: "1:199766521789:web:256b37c1addbf9c04284d4",
+    measurementId: "G-CTW2EPRCRE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,19 +7,31 @@ import { deletePost } from '../redux/modules/posts';
 import Button from '../components/button/Button';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { db } from '../firebase/firebase';
+import {collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc} from 'firebase/firestore';
 
 const MainPage = (props) => {
     const data = useSelector(state => state.posts.post_list);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    useEffect(()=>{
+    const firebaseTest = async() => {
+        const docRef = await getDocs(collection(db, 'post_list'));
+        docRef.forEach((doc)=> {
+            console.log(`${doc.id} => ${doc.data()}`);
+        })
+
+    }
+
+    firebaseTest();
+
+    useEffect(() => {
         console.log(data);
     }, [data]);
 
     return (
         <div>
-            <Header btn_text={<AddIcon/>} btn_action={()=>navigate('/posting')}/>
+            <Header btn_text={<AddIcon />} btn_action={() => navigate('/posting')} />
 
             {data.map((post) => {
                 return (
@@ -27,7 +39,7 @@ const MainPage = (props) => {
                         <StyledH1 onClick={() => {
                             navigate('/post/' + post.id);
                         }}>{post.title}</StyledH1>
-                        <Button text={<DeleteIcon/>} action={() => { dispatch(deletePost(post.id)); }}/>
+                        <Button text={<DeleteIcon />} action={() => { dispatch(deletePost(post.id)); }} />
                     </PostWrapper>
                 );
             })}
